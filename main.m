97dcf18276cc134612ee99ea809b3708ea57163a
a-katayama6 <aoto.k63@gmail.com>
2025-04-17 14:34:29 +0900

    step1修正

--- main.m
+++ main.m
@@ -1,34 +1,37 @@
 % 信号の読み込み
 filename = '美声.wav';
-[audioData, samplingRate] = audioread(filename);
+[audioData, Fs] = audioread(filename);
 %sound(audioData, samplingRate);
 
 % 短時間信号への分割
-frameLength_sec = 0.02;     % フレーム長
-frameShift_sec = 0.01;      % フレームシフト長
+%frameLength_sec = 0.02;     % フレーム長
+%frameShift_sec = 0.01;      % フレームシフト長
 
 % サンプル単位のフレーム長とシフト長に変換
-frameLength = round(frameLength_sec * samplingRate);
-frameShift = round(frameShift_sec * samplingRate);
+%frameLength = round(frameLength_sec * Fs);
+%frameShift = round(frameShift_sec * Fs);
+
+windowLength = 2 ^ 9;               %窓長
+shiftWidth = windowLength / 2;      %シフト幅
 
 % 信号の長さを取得
-signalLength = length(audioData);
+sigLen = length(audioData);
 
 % フレーム数を計算
-numFrames = floor((signalLength - frameLength) / frameShift) + 1;
+numFrames = floor((sigLen - windowLength) / shiftWidth) + 1;
 
 % 短時間信号を格納する行列を初期化
-frames = zeros(frameLength, numFrames);
+frames = zeros(windowLength, numFrames);
 
 % フレームの抽出
 for i = 1:numFrames
-    startIndex = (i - 1) * frameShift + 1;
-    endIndex = startIndex + frameLength -1;
+    startIdx = (i - 1) * shiftWidth + 1;
+    endIdx = startIdx + windowLength -1;
     
-    if endIndex > signalLength
-        frames(:, i) = [audioData(startIndex:signalLength); zeros(endIndex - signalLength, 1)];
+    if endIdx > sigLen
+        frames(:, i) = [audioData(startIdx:signalLength); zeros(endIdx - sigLen, 1)];
     else
-        frames(:, i) = audioData(startIndex:endIndex);
+        frames(:, i) = audioData(startIdx:endIdx);
     end
 end
 
@@ -36,12 +39,13 @@ end
 %disp(['各フレームの長さ', num2str(size(frames, 1)), 'サンプル']);
 
 % 窓関数（ハン窓）の実装
-hanningWindow = hamming(frameLength);
+n = 0:windowLength-1;
+hanWin = 0.5 - 0.5 * cos((2 * pi * n) / (windowLength -1));
+hanWin = hanWin(:);
 
 % ハン窓を信号に乗算
 windowedFrames = zeros(size(frames));
 for i = 1:size(frames,2)
-    windowedFrames(:, i) = frames(:, i) .* hanningWindow;
+    windowedFrames(:, i) = frames(:, i) .* hanWin;
 end
-
 
